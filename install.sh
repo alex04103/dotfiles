dfa6882561cc9506fb04941a95581f18d3802846
Compilenix <Compilenix@compilenix.org>
2015-06-19 16:06:36 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 git clone git://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
 $HOME/.homesick/repos/homeshick/bin/homeshick clone compilenix/dotfiles-flat235
+vim +PluginInstall +qall
